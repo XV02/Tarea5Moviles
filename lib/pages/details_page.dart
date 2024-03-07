cc143e2c56ec0e849e4cf877d71e8c02fea672e4
XV02 <lghhssi@gmail.com>
2024-03-07 12:47:33 -0600

    feat: Updated widget organization

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -19,37 +19,59 @@ class DetailsMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          context.watch<ForageListProvider>().selectedForage.name!,
-          style: const TextStyle(fontSize: 24),
-        ),
-      ),
-      Expanded(
-        child: ListView(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.add_location),
-              title: Text(
-                  context.watch<ForageListProvider>().selectedForage.location!),
-            ),
-            ListTile(
-              leading: const Icon(Icons.date_range),
-              title: Text(
-                  context.watch<ForageListProvider>().selectedForage.isInSeason!
-                      ? 'Curently in season'
-                      : 'Not in season'),
-            ),
-            ListTile(
-              leading: const Icon(Icons.notes),
-              title: Text(
-                  context.watch<ForageListProvider>().selectedForage.notes!),
-            ),
-          ],
-        ),
-      ),
+    return const Column(children: [
+      NameTitle(),
+      DetailsList(),
     ]);
+  }
+}
+
+class DetailsList extends StatelessWidget {
+  const DetailsList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.add_location),
+            title: Text(
+                context.watch<ForageListProvider>().selectedForage.location!),
+          ),
+          ListTile(
+            leading: const Icon(Icons.date_range),
+            title: Text(
+                context.watch<ForageListProvider>().selectedForage.isInSeason!
+                    ? 'Curently in season'
+                    : 'Not in season'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.notes),
+            title:
+                Text(context.watch<ForageListProvider>().selectedForage.notes!),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NameTitle extends StatelessWidget {
+  const NameTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text(
+        context.watch<ForageListProvider>().selectedForage.name!,
+        style: const TextStyle(fontSize: 24),
+      ),
+    );
   }
 }
